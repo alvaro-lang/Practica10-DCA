671fb6f7d0d184096edcbf13f71ef91c9018949c
Alvaro Pastor <alvaropers96@gmail.com>
2022-12-19 16:09:45 +0100

    cambiad comentarios del programa a español

--- Calculadora.py
+++ Calculadora.py
@@ -3,27 +3,28 @@ from colorama import Fore, Style
 # Funcion de suma
 def sum(num1, num2):
 
-    print(Fore.GREEN + "\nResult: " + str(float(num1) + float(num2)))
+    print(Fore.GREEN + "\nResultado: " + str(float(num1) + float(num2)))
     print(Style.RESET_ALL, end="")
 
 # Funcion de resta
 def subtract(num1, num2):
     
-    print(Fore.GREEN + "\nResult: " + str(float(num1) - float(num2)))
+    print(Fore.GREEN + "\nResultado: " + str(float(num1) - float(num2)))
     print(Style.RESET_ALL, end="")
 
 # Funcion de multiplicacion
 def multiply(num1, num2):
 
-    print(Fore.GREEN + "\nResult: " + str(float(num1)* float(num2)))
+    print(Fore.GREEN + "\nResultado: " + str(float(num1)* float(num2)))
     print(Style.RESET_ALL, end="")    
 
 # Funcion de division
 def divide(num1, num2):
     
-    print(Fore.GREEN + "\nResult: " + str(float(num1) / float(num2)))
+    print(Fore.GREEN + "\nResultado: " + str(float(num1) / float(num2)))
     print(Style.RESET_ALL, end="")
 
+# Funcion main
 def main():
 
     num1 = ""
@@ -31,26 +32,26 @@ def main():
     operator = ""
 
     print(Fore.YELLOW + "\n##############")
-    print(Fore.YELLOW + "# CALCULATOR #")
+    print(Fore.YELLOW + "# CALCULADORA #")
     print(Fore.YELLOW + "##############\n")
     print(Style.RESET_ALL, end="")
 
     while not num1.isdigit():
-        print("Introduce first number: ", end="")
+        print("Introduce primer numero: ", end="")
         num1 = input()
         if not num1.isdigit():
             print(Fore.RED + "Error. Debes introducir un numero.\n")
             print(Style.RESET_ALL, end="")
 
     while not num2.isdigit():
-        print("Introduce second number: ", end="")
+        print("Introduce segundo numero: ", end="")
         num2 = input()
         if not num2.isdigit():
             print(Fore.RED + "Error. Debes introducir un numero.\n")
             print(Style.RESET_ALL, end="")
 
     while operator != "+" and operator != "-" and operator != "*" and operator != "/":
-        print("Introduce operator: ", end="")
+        print("Introduce operador: ", end="")
         operator = input()
         if operator != "+" and operator != "-" and operator != "*" and operator != "/":
             print(Fore.RED + "Error. Debes introducir uno de los siguientes operadores: + , - , * , /\n")
@@ -59,7 +60,7 @@ def main():
     if operator == "+":
         sum(num1, num2)
     elif operator == "-":
-        subtract(num1, num2)
+        sum(num1, num2)
     if operator == "*":
         multiply(num1, num2)
     elif operator == "/":
